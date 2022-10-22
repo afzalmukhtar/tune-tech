ce7122a7a2100bc0bd8544bc8d4fc47f07ee579e
Afzal Mukhtar <67794247+afzalmukhtar@users.noreply.github.com>
2022-10-22 15:28:27 +0530

    Update FeatureExtractor.py

--- lib/FeatureExtractor.py
+++ lib/FeatureExtractor.py
@@ -2,9 +2,9 @@
 
 import nltk
 
-# nltk.download('punkt')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 
 from keras.models import load_model
 import pickle
