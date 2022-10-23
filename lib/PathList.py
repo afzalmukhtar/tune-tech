1bb2494e9fdc650d23e18f7eeb10c9ccf5db6e57
Afzal Mukhtar <67794247+afzalmukhtar@users.noreply.github.com>
2022-10-23 17:32:29 +0530

    Update PathList.py

--- lib/PathList.py
+++ lib/PathList.py
@@ -1,19 +1,19 @@
 #!/bin/python3
 
-VARIABLES_PATH = "./Models/variables.pickle"
+VARIABLES_PATH = "lib/Models/variables.pickle"
 
 # FaceEmotion Variables
-FER_MODELS_PATH = ["./Models/FER_part_1.h5", "./Models/FER_part_2.dat"]
-FRONT_FACE_CASCADE = "./Models/haarcascade_frontalface_default.xml"
-TEST_IMAGE = "./Models/Test image.jpg"
+FER_MODELS_PATH = ["lib/Models/FER_part_1.h5", "lib/Models/FER_part_2.dat"]
+FRONT_FACE_CASCADE = "lib/Models/haarcascade_frontalface_default.xml"
+TEST_IMAGE = "lib/Models/Test image.jpg"
 
 # SongAudio Variables
 COLUMN_ORDER = ['name', 'album', 'artist', 'id', 'release_date', 'popularity', 'duration_ms', 'danceability', 'acousticness', 'energy', 'instrumentalness', 'liveness', 'valence', 'loudness', 'speechiness', 'tempo', 'key', 'time_signature']
-AUDIO_MODELS_PATH = ["./Models/Audio_Mood.dat"]
+AUDIO_MODELS_PATH = ["lib/Models/Audio_Mood.dat"]
 
 
 # SongLyrics Variables
-LYRICS_MODELS_PATH = ["./Models/Lyrics_Mood_part_1.dat", "./Models/Lyrics_Mood_part_2.h5", "./Models/Lyrics_Mood_part_3.h5"]
+LYRICS_MODELS_PATH = ["lib/Models/Lyrics_Mood_part_1.dat", "lib/Models/Lyrics_Mood_part_2.h5", "lib/Models/Lyrics_Mood_part_3.h5"]
 
 # SongRecommender Variables
-SONG_LIST_PATH = "./Models/SongList.csv"
+SONG_LIST_PATH = "lib/Models/SongList.csv"
